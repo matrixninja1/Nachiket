16fb2f479fad49b00f53a1a201fc7c79bdb4f687
Krishna <Krishna@KRISHNA>
2024-07-09 15:30:49 +0530

    By Nachiket

---
+++ GithubProject2/src/com/cjc/HTML.java
@@ -0,0 +1,15 @@
+package com.cjc;
+
+public class HTML {
+
+	public static void main(String args[]) {
+		
+		System.out.println("Nachiket");
+		
+		int a=20;
+		int b=20;
+        int c = a+b;
+        System.out.println(c);
+	}
+
+}
